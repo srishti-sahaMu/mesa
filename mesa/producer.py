256edfc603113543c6afafdcb1a5623ef60dde9b
srishti-sahaMu <42168123+srishti-sahaMu@users.noreply.github.com>
2018-08-07 18:12:44 +0530

    Update producer.py

--- mesa/producer.py
+++ mesa/producer.py
@@ -1,35 +1,18 @@
 # producer.py
 
-import time
-import cv2
-from kafka import SimpleProducer, KafkaClient
+from kafka import SimpleProducer, KafkaClient, KafkaProducer
 #  connect to Kafka
 kafka = KafkaClient('localhost:9092')
-producer = SimpleProducer(kafka)
+producer = KafkaProducer(kafka)
 # Assign a topic
-topic = 'my-topic'
+topic = 'testtopic1'
 
-def video_emitter(video):
-    # Open the video
-    video = cv2.VideoCapture(video)
+def msg_emitter(df):
+    
     print(' emitting.....')
-
-    # read the file
-    while (video.isOpened):
-        # read the image in each frame
-        success, image = video.read()
-        # check if the file has read to the end
-        if not success:
-            break
-        # convert the image png
-        ret, jpeg = cv2.imencode('.png', image)
-        # Convert the image to bytes and send to kafka
-        producer.send_messages(topic, jpeg.tobytes())
-        # To reduce CPU usage create sleep time of 0.2sec  
-        time.sleep(0.2)
-    # clear the capture
-    video.release()
+    # Convert the test string to bytes and send to kafka
+    df_bytes = df.encode()
+    producer.send(topic, df_bytes)
+    producer.flush()
     print('done emitting')
 
-if __name__ == '__main__':
-    video_emitter('video.mp4')
